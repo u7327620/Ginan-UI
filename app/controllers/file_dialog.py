e22c113cd0fb449e6e7ad8119de275f57eee281e
ketan <u7798820@anu.edu.au>
2025-04-16 15:39:32 +1000

    create a naive historical view

--- app/controllers/file_dialog.py
+++ app/controllers/file_dialog.py
@@ -1,5 +1,6 @@
 from app.views.main_window_ui import Ui_MainWindow
-from PySide6.QtWidgets import QFileDialog
+from PySide6.QtWidgets import QFileDialog, QWidget, QVBoxLayout
+from app.views.history_window_ui import Ui_HistoryWindow
 
 class FileDialogController:
     def __init__(self, ui:Ui_MainWindow, model): # Model empty for now
@@ -10,7 +11,20 @@ class FileDialogController:
 
     def setup_file_dialog(self):
         self.ui.inputDataButton.clicked.connect(self.get_directory)
+        self.ui.VisualisationButton.clicked.connect(self.open_history_window)
 
     def get_directory(self):
         dialog = QFileDialog().getExistingDirectory(caption='Select root input directory')
         self.ui.inputPathLineEdit.setText(str(dialog))
+
+    def open_history_window(self):
+        for i in reversed(range(self.ui.infoGrid.count())):
+            widget_to_remove = self.ui.infoGrid.itemAt(i).widget()
+            if widget_to_remove is not None:
+                widget_to_remove.setParent(None)
+
+        self.ui_history_window = Ui_HistoryWindow()
+        self.history_widget = QWidget()
+        self.ui_history_window.setupUi(self.history_widget)
+
+        self.ui.infoGrid.addWidget(self.history_widget, 0, 0, 1, 2)
