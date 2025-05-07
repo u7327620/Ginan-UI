7b9803649fe9302084596ab9f0c4523244f39383
u7327620 <u7327620@anu.edu.au>
2025-05-07 11:49:48 +1000

    Updated to executable model

--- app/models/file_integrity.py
+++ app/models/file_integrity.py
@@ -5,16 +5,20 @@ import shutil
 import subprocess
 
 
-def get_pea_exec() -> pathlib.Path:
-    if platform.system().lower() == "linux":
-        try:
-            path = pathlib.Path(f"{os.getcwd()}/../resources/ginan.AppImage")
-            subprocess.run(path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            return path
-        except FileNotFoundError:
-            raise RuntimeError("Ginan executable for linux not found, check resources folder has ginan.AppImage")
+def get_pea_exec() -> pathlib.Path | [str]:
+
+    # PEA available on PATH
+    if shutil.which("pea"):
+        executable = ["pea"]
+
+    # Attempts executing AppImage natively
+    elif platform.system().lower() == "linux":
+        executable = pathlib.Path(f"{os.getcwd()}/../resources/ginan.AppImage")
+
+    # Uses/Installs WSL and Ubuntu before executing PEA
+    ## TODO - Test and complete on windows system
     elif platform.system().lower() == "windows":
-        # Check if wsl is available
+        # Check if WSL is installed
         if not shutil.which("wsl"):
             result = subprocess.run(['powershell.exe', '-Command',
                                      'Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart'],
@@ -23,16 +27,35 @@ def get_pea_exec() -> pathlib.Path:
                 print("WSL enabled successfully. Please restart your computer.")
             else:
                 raise RuntimeError(f"Error enabling WSL: {result.stderr}")
-        # Ubuntu and run commands
+
+        # Check if Ubuntu is installed
         try:
             subprocess.call(['wsl', '--install', '-d', 'Ubuntu'], text=True)
-            ## TODO - See how to call appImage after this step...
             return pathlib.Path(f"{os.getcwd()}/../resources/ginan.AppImage")
         except subprocess.CalledProcessError as e:
             print(f"Error installing Ubuntu: {e.stderr}")
             raise RuntimeError("Error installing Ubuntu")
+
+        # Execute PEA AppImage
+        pass
+
+    # Unknown system
     else:
         raise RuntimeError("Unsupported platform: " + platform.system())
+
+    # Test and return executable
+    if not executable:
+        raise RuntimeError("No executable found yet running on supported system: " + platform.system())
+    else:
+        _test_executable(executable)
+        return executable
+
+def _test_executable(pea_exec):
+    try:
+        subprocess.call(pea_exec, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running PEA executable: {e.stderr}")
+        raise RuntimeError("Error running PEA executable")
 
 if __name__ == "__main__":
     print(get_pea_exec().resolve())
