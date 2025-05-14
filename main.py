9086af59056a1906a4d5853b2457f04e6d6803d8
u7327620 <u7327620@anu.edu.au>
2025-05-14 12:30:44 +1000

    Helper to automatically compile UI whilst developing

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from app.main_window import MainWindow
+from utils.helpers import compile_ui
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    compile_ui()
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
