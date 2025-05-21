cda729f0399b9ed69ff8abfaedd7cb526388ff98
Ryan <87843426+u7327620@users.noreply.github.com>
2025-05-21 14:23:27 +1000

    Scrum 135 create test cases for scrum 103 implement the example configs (#30)
    * Working on tests, moving to another branch
    
    * EUREKA! IT WORKS!
    
    * Working execution test

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import unittest
 from importlib.resources import files
 from app.models.execution import Execution
@@ -10,10 +12,23 @@ class TestExecution(unittest.TestCase):
 
     def test_execute_config(self):
         execution = Execution(files("tests.resources").joinpath("ppp_example.yaml"), get_pea_exec())
+        folder_path = str(files("tests.resources").joinpath("output"))
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
+
         alterations = {"outputs.outputs_root": files("tests.resources").joinpath("output"),
-                       "inputs.inputs_root": files("tests.resources").joinpath("input")}
+                       "inputs.inputs_root": files("tests.resources").joinpath("inputData/products")}
         for key, value in alterations.items():
             execution.edit_config(key, str(value))
 
+        execution.write_config()
+
         execution.execute_config()
-        # TODO - Verify output when execution is complete.
+        self.assertTrue(os.listdir(folder_path), "Output folder should not be empty")
