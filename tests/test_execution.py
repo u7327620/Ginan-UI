50cc38158835f5e35be5819ce2a7bb35de675732
u7327620 <u7327620@anu.edu.au>
2025-05-21 15:48:50 +1000

    Automatically downloads data for tests

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 import unittest
 from importlib.resources import files
 from app.models.execution import Execution
@@ -11,6 +12,14 @@ class TestExecution(unittest.TestCase):
         self.assertFalse(execution.config.values() == {}, "Config isn't empty")
 
     def test_execute_config(self):
+        data_folder = files("tests.resources").joinpath("inputData/data")
+        product_folder = files("tests.resources").joinpath("inputData/products")
+        if not len(os.listdir(str(data_folder))) > 3:
+            subprocess.call("./getData.sh", shell=True, text=True, cwd=str(data_folder))
+
+        if not len(os.listdir(str(product_folder))) > 3:
+            subprocess.call("./getProducts.sh", shell=True, text=True, cwd=str(product_folder))
+
         execution = Execution(files("tests.resources").joinpath("ppp_example.yaml"), get_pea_exec())
         folder_path = str(files("tests.resources").joinpath("output"))
         for filename in os.listdir(folder_path):
@@ -27,8 +36,6 @@ class TestExecution(unittest.TestCase):
                        "inputs.inputs_root": files("tests.resources").joinpath("inputData/products")}
         for key, value in alterations.items():
             execution.edit_config(key, str(value))
-
-        execution.write_config()
 
         execution.execute_config()
         self.assertTrue(os.listdir(folder_path), "Output folder should not be empty")
